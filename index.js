86247ca58448a556e667a90b119459f39bb8fa8c
Chris Beyer <beyercorpuz@gmail.com>
2017-06-04 23:56:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function theBeatlesPlay(musicians, instruments){
+    var  music = []
+    // var   musicians = ["John", "Paul","George", "Ringo"];
+    // var   instruments=["Guitar", "Bass", "Drums", "Piano"];
+
+    for (var i = 0; i < musicians.length; i++) {
+      music.push(musicians[i] + " plays "+  instruments[i]);
+
+
+
+    }
+
+    return music
+}
+
+function johnLennonFacts(jlfacts){
+var facts=[  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"]
+//  i=0
+  while (var i = 0;i < 8) {
+      text += "The number is " + i;
+      i++;
+  }
+
+return jlfacts
+
+}
+
+function iLoveTheBeatles(num)
+var Love = []
+do while (var i = 0; i < num.length; i++){
+  num[i]
+}
